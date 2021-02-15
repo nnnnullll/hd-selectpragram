d976fb30ca804d620cd41bec89e2a609336b048b
Rebecca-zy <273146618@qq.com>
2021-02-15 14:51:14 +0800

    结构修改

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,9 @@
 package com.example.demo;
-
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@MapperScan("com.example.web1.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
